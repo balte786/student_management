10bf33598b4d42f45af623b0c737b41b84922ca2
syedakhter <dev.syed.akhter@gmail.com>
2021-06-23 14:49:21 -0400

    latest updates

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 /*Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
 
