19cc88fcb7200b282321c3802d5df56b549f8c64
iqbalbalte <iqbalbalte786@gmail.com>
2021-06-25 17:21:46 +0500

    latest changes

--- resources/views/admin-quota-upload.blade.php
+++ resources/views/admin-quota-upload.blade.php
@@ -9,85 +9,74 @@
             </ul>
         </div>
         <div class="separator-breadcrumb border-top"></div>
-        <div class="row">
+        <div class="row"><!-- start of row-->
             <div class="col-lg-12 col-md-12">
-                <h2 class=" mb-6">Admission Quota Management</h2>
-                <hr>
-                <a href="admin-quota-upload.php"><button class="btn btn-primary" type="button">ADD NEW YEARLY QUOTA</button></a>
-                <hr>
-                <!--begin::form-->
-                <div class="row"><!-- start of row-->
-                    <div class="col-lg-12 col-md-12">
-                        <!--table start-->
-                        <div class="card text-left">
+                <!--table start-->
+                <div class="card text-left">
 
-                            <div class="card-body">
-                                <h4 class="card-title mb-3">Adminission Quota Upload</h4>
+                    <div class="card-body">
+                        <h4 class="card-title mb-3">Adminission Quota Upload</h4>
 
-                                <div class="row">
-                                    <div class="col-lg-12 col-md-12">
-                                        <p>Download the Quota Template and complete accordingly</p>
-                                        <button class="btn btn-primary btn-icon m-1" type="button"><span class="ul-btn__icon"><i class="i-File-Excel"></i></span><span class="ul-btn__text">DOWNLOAD UNIVERSIY QUOTA TEMPLATE</span></button>
-                                        <button class="btn btn-primary btn-icon m-1" type="button"><span class="ul-btn__icon"><i class="i-File-Excel"></i></span><span class="ul-btn__text">DOWNLOAD SCHOOL OF HEALTH TECHNOLOGY QUOTA TEMPLATE</span></button>
-
-                                    </div>
-                                    <div class="col-lg-12 col-md-12">
-                                        <p>Upload the completed Quota Template and select the year</p>
-                                        <form action="{{ URL::to('admin/admin-quota-upload') }}" method="POST" enctype="multipart/form-data">
-                                            @csrf
-                                            <div class="row">
-                                                <div class="col-md-4 form-group mb-3 input-group">
-
-                                                    <select name="year" class="form-control" >
-                                                        <option selected>Select Year</option>
-                                                        <option value="2021">2021</option>
-                                                        <option value="2022">2022</option>
-                                                        <option value="2023">2023</option>
-                                                        <option value="2024">2024</option>
-                                                        <option value="2025">2025</option>
-                                                    </select>
-                                                </div>
-                                                <div class="col-md-4 form-group mb-3 input-group">
-
-                                                    <select name="category_id" class="form-control" >
-                                                        <option selected>Institution Category</option>
-                                                        <option value="1">University</option>
-                                                        <option value="2">School of Health Technology</option>
-
-                                                    </select>
-                                                </div>
-                                                <div class="col-md-4 form-group mb-3 input-group">
-
-                                                    <div class="custom-file">
-                                                        <input class="custom-file-input" name="file" id="inputGroupFile02" type="file" />
-                                                        <label class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Choose file</label>
-                                                    </div>
-                                                   {{-- <div class="input-group-append"><span class="input-group-text" id="inputGroupFileAddon02">Upload</span></div>--}}
-                                                </div>
-
-                                                <div class="col-md-12">
-                                                    <button class="btn btn-primary">UPLOAD EXCEL TEMPLATE</button>
-                                                </div>
-
-                                                {{--<div class="col-md-12">
-                                                    <a href="school-index-upload-2.php" target="_self" class="btn btn-primary">UPLOAD EXCEL TEMPLATE</a>
-                                                </div>--}}
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-
-
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <p>Download the Quota Template and complete accordingly</p>
+                                <button class="btn btn-primary btn-icon m-1" type="button"><span class="ul-btn__icon"><i class="i-File-Excel"></i></span><span class="ul-btn__text">DOWNLOAD UNIVERSIY QUOTA TEMPLATE</span></button>
+                                <button class="btn btn-primary btn-icon m-1" type="button"><span class="ul-btn__icon"><i class="i-File-Excel"></i></span><span class="ul-btn__text">DOWNLOAD SCHOOL OF HEALTH TECHNOLOGY QUOTA TEMPLATE</span></button>
 
                             </div>
+                            <div class="col-lg-12 col-md-12">
+                                <p>Upload the completed Quota Template and select the year</p>
+                                <form action="{{ URL::to('admin/quota-import') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-4 form-group mb-3 input-group">
 
+                                            <select name="year" class="form-control" >
+                                                <option selected>Select Year</option>
+                                                <option value="2021">2021</option>
+                                                <option value="2022">2022</option>
+                                                <option value="2023">2023</option>
+                                                <option value="2024">2024</option>
+                                                <option value="2025">2025</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4 form-group mb-3 input-group">
+
+                                            <select name="category_id" class="form-control" >
+                                                <option selected>Institution Category</option>
+                                                <option value="1">University</option>
+                                                <option value="2">School of Health Technology</option>
+
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4 form-group mb-3 input-group">
+
+                                            <div class="custom-file">
+                                                <input class="custom-file-input" name="file" id="inputGroupFile02" type="file" />
+                                                <label class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Choose file</label>
+                                            </div>
+                                            {{-- <div class="input-group-append"><span class="input-group-text" id="inputGroupFileAddon02">Upload</span></div>--}}
+                                        </div>
+
+                                        <div class="col-md-12">
+                                            <button class="btn btn-primary">UPLOAD EXCEL TEMPLATE</button>
+                                        </div>
+
+                                        {{--<div class="col-md-12">
+                                            <a href="school-index-upload-2.php" target="_self" class="btn btn-primary">UPLOAD EXCEL TEMPLATE</a>
+                                        </div>--}}
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                    <!--table end-->
-                </div>
 
-                <!-- end::form -->
+
+
+                    </div>
+
+                </div>
             </div>
+            <!--table end-->
         </div>
         <!-- end of row-->
 
